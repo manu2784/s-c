396ed218ec1004a69c45e30b01d4d65f4c1be92c
malleshashnikar <manu2784@hotmail.com>
2017-09-22 21:38:04 +0100

    additional error reporting

--- data/stocks/update_stocks.php
+++ data/stocks/update_stocks.php
@@ -11,9 +11,10 @@ if (!file_exists($update_dir)) {
 }
 
 // error variables
-$total_files_downloaded=0;
-$total_symbols=0;
-$number_symbols_used=0;
+$total_files_downloaded=0;                     // actual number of html files downloaded
+$total_symbols=0;							   // total number of symbol in the db	
+$number_symbols_used=0;						   // symbols used to download files
+$no_data_symbols=0;							   // symbol used but no data available	
 $error_count=0;
 $error_content="";
 $err_dir=ERRORLOG."/".date('d_m_y').".csv";
@@ -45,7 +46,7 @@ while($row=$get_record->fetch_array())
  	  	 	
  	  	 		foreach ($symbols_downloaded as $sym) 
  	  	 		{
- 	  	 			$download_record['$symbol'][]=$sym;
+ 	  	 			$download_record[$symbol][]=$sym;
  	  	 			
  	  	 		}
 
@@ -56,8 +57,29 @@ while($row=$get_record->fetch_array())
  }
 
 
+// Retrieving symbols for which no data exist for any time intervals in the given date range
+
+foreach ($download_record as $sym => $sym_record) 
+{
+
+	$j=true;
+	foreach ($sym_record as $record)
+	{	
+
+		$x=$record[0];
+		$j=($j && $x);
+
+	}
+			if(!$j) {
+						$no_data_symbols++;
+		           }
+
+}
+
 // Retrieving individual download records and error if any 
+
 foreach ($download_record as $sym_record) {
+
 		
 	foreach ($sym_record as $record) {
 					if($record[0])
@@ -66,7 +88,7 @@ foreach ($download_record as $sym_record) {
 
 						} elseif (!$record[0]) {
 							$error_count++;
-							$error_content.=$record[3].",".$record[1].",".$record[2]."\n";
+							$error_content.=$record[3].",".$record[1].",".$record[2].",".$record[6]."\n";
 							
 						}			
 
@@ -96,9 +118,7 @@ if($total_symbols==$number_symbols_used && $error_count>0) {
 		     }
 
 echo "Total Files Downloaded= ".$total_files_downloaded."<br>"."Number of Errors= ".$error_count."<br>"."Total No Symbol=".$total_symbols."<br>"
-."Number of Symbol Downloaded=".$number_symbols_used."<br>"."Update Process=".$update_process."<br>";
-
-
+."Number of Symbol Downloaded=".$number_symbols_used."<br>"."Number of Symbol with no data= ".$no_data_symbols."<br>"."Update Process=".$update_process."<br>";
 
 
 
